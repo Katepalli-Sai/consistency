927fc31c4b26f2192f21a494b132cd116e8a7703
KatepalliSaiganesh <161560846+Katepalli-Sai@users.noreply.github.com>
2026-01-02 22:19:42 +0530

    Fix result array size and loop in ZigZagDifference
    Updated result array size and loop iteration for ZigZag calculation.

--- ZigZagDifference.java
+++ ZigZagDifference.java
@@ -133,8 +133,8 @@ class Main {
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        int res[] = new int[n-1];
-        for(int i=0;i<n-1;i++){
+        int res[] = new int[n];
+        for(int i=0;i<n;i++){
             if(i%2==0){
                 res[i] = arr[i+1]-arr[i];
             }else{
